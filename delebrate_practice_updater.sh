879addf9cc8a42a5fd476b1f486221df5068072e
Solomon Udoh <solomonlazio@gmail.com>
2019-04-11 16:04:21 +0100

    update python delebrate practice)

---
+++ delebrate_practice_updater.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+#update every four days
+#Author: Solomon Amos
+
+cd ~/projects/My_python_deliberate_practice
+#git init
+git add .
+git commit -m "update python delebrate practice)"
+git remote add origin https://github.com/udohsolomon/My_python_deliberate_practice
+git push -u origin master
+git config credential.helper store
