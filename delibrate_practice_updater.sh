ffbd5e0a225b364a60df473e7eeafed0d2a62b74
Solomon Udoh <solomonlazio@gmail.com>
2019-04-11 16:59:25 +0100

    update python delibrate practice)

--- delibrate_practice_updater.sh
+++ delibrate_practice_updater.sh
@@ -6,7 +6,7 @@
 cd ~/projects/My_python_deliberate_practice
 #git init
 git add .
-git commit -m "update python delebrate practice)"
-git remote add origin https://github.com/udohsolomon/My_python_deliberate_practice
+git commit -m "update python delibrate practice)"
+# git remote add origin https://github.com/udohsolomon/My_python_deliberate_practice
 git push -u origin master
 git config credential.helper store
